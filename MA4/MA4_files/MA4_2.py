9c9cbe8e838a61a5beda7bd360b8238b7288b71c
Olivia Rudqvist <olru0352@atterbom.it.uu.se>
2021-10-15 12:23:45 +0200

    uppdated files

--- MA4/MA4_files/MA4_2.py
+++ MA4/MA4_files/MA4_2.py
@@ -1,23 +1,37 @@
 #!/usr/bin/env python3
-
 from integer import Integer
-
+from time import perf_counter as pc
+import matplotlib.pyplot as plt
+def fib_py(n):
+	if n<=1:
+		return n
+	else:
+		return(fib_py(n-1)+fib_py(n-2))
 def main():
-	f = Integer(5)
+	f=Integer(5)
 	print(f.get())
 	f.set(7)
 	print(f.get())
-	#f = Integer(n)
-	#f.fib()
-	n=5
-	fi = fib_py(n)
+	n=47
+	start_c=pc()
+	f=Integer(n)
+	f.fib()
+	end_c=pc()
+	print(f.fib())
+	print("time c++",end_c -start_c)
+	start=pc()
+	n=6
+	fi=fib_py(n)
+	end=pc()
 	print(fi)
-def fib_py(n):
-	if n<= 1:
-		return n
-	else:
-		return(fib_py(n-1)+fib_py(n-2)
-
-
-if __name__ == '__main__':
+	print("time pyton",end-start)
+	for i in range(30,48):
+		start_cpp=pc()
+		f=Integer(i)
+		f.fib()
+		end_cpp=pc()
+		plt.scatter(i,end_cpp -start_cpp,color='blue')
+	print("time  for loop of fib",end_cpp -start_cpp)
+	plt.savefig("plotfibtime.png")
+if __name__=='__main__':
 	main()
--- MA4/MA4_files/integer.cpp
+++ MA4/MA4_files/integer.cpp
@@ -1,32 +1,39 @@
 #include <cstdlib>
-// Integer class 
+// Integer class
 
 class Integer{
 	public:
 		Integer(int);
 		int get();
+		int fib();
 		void set(int);
 	private:
 		int val;
+		int fib_c(int);
 	};
- 
 Integer::Integer(int n){
 	val = n;
 	}
- 
 int Integer::get(){
 	return val;
 	}
- 
 void Integer::set(int n){
 	val = n;
 	}
-
-
+int Integer::fib_c(int n){
+	if( n<=1)
+		return n;
+	else
+		return (fib_c(n-1)+fib_c(n-2));
+}
+int Integer::fib(){
+	return fib_c(val);
+}
 extern "C"{
 	Integer* Integer_new(int n) {return new Integer(n);}
 	int Integer_get(Integer* integer) {return integer->get();}
 	void Integer_set(Integer* integer, int n) {integer->set(n);}
+	int Integer_fib(Integer* integer){return integer->fib();}
 	void Integer_delete(Integer* integer){
 		if (integer){
 			delete integer;
