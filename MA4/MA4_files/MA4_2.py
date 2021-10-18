8b2b540dda5f0863e4cd6016ccd32f6c109d38c2
Olivia Rudqvist <olru0352@atterbom.it.uu.se>
2021-10-18 14:59:48 +0200

    uppdated py file

--- MA4/MA4_files/MA4_2.py
+++ MA4/MA4_files/MA4_2.py
@@ -15,9 +15,9 @@ def main():
 	n=47
 	start_c=pc()
 	f=Integer(n)
-	f.fib()
+	fib47=f.fib()
 	end_c=pc()
-	print(f.fib())
+	print(fib47)
 	print("time c++",end_c -start_c)
 	start=pc()
 	n=6
@@ -25,11 +25,15 @@ def main():
 	end=pc()
 	print(fi)
 	print("time pyton",end-start)
-	for i in range(30,48):
+	for i in range(30,45):
 		start_cpp=pc()
 		f=Integer(i)
 		f.fib()
 		end_cpp=pc()
+		start_p = pc()
+		fib_py(i)
+		end_p=pc()
+		plt.scatter(i,end_p-start_p, color='red')
 		plt.scatter(i,end_cpp -start_cpp,color='blue')
 	print("time  for loop of fib",end_cpp -start_cpp)
 	plt.savefig("plotfibtime.png")
